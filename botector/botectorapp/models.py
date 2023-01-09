29266d354021d983b6e04b3c0512763a232b559f
Usman Fawad <ufawad0@gmail.com>
2023-01-09 18:13:05 +0500

    first Migration made

--- botector/botectorapp/models.py
+++ botector/botectorapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+#Just creating a random model for learning purpose.
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
